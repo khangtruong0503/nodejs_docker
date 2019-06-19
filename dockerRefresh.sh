b1c4749a71e8e3df2d286f30001f7e1846741070
Unknown <khangzhang0503@gmail.com>
2019-06-19 21:59:48 +0700

    add docker file

---
+++ dockerRefresh.sh
@@ -0,0 +1,9 @@
+export IMAGE_NAME='test'
+export PORT=5000
+
+# build image
+sudo docker build -t $IMAGE_NAME .
+
+# run image
+echo Server is running on port: $PORT
+sudo docker run -p $PORT:3000 -it $IMAGE_NAME
